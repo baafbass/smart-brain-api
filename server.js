8303ca678b5b65e50a13b1e2c6b7ea9ea6b231ba
Farid <abdoulfaridbassirou7898@gmail.com>
2024-01-21 19:41:08 +0300

    /signin and /register

--- server.js
+++ server.js
@@ -1,9 +1,56 @@
-import express from 'express'
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express()
 
+app.use(bodyParser.json());
+
+const database = {
+	users : [
+      {
+      	id: '123',
+      	name: 'farid',
+      	email: 'farid@gmail.com',
+      	password: 'bass1971',
+      	entries: 0,
+      	joined: new Date()
+      },
+      {
+      	id: '124',
+      	name: 'Top G',
+      	email: 'topg@gmail.com',
+      	password: 'junky',
+      	entries: 0,
+      	joined: new Date()
+      }
+	]
+}
+
 app.get('/',(req,res)=>{
-	res.send('This is working');
+	res.json(database.users);
+})
+
+app.post('/signin',(req,res)=>{
+	if(req.body.email === database.users[0].email && req.body.password === database.users[0].password)
+	{
+		res.json('success');
+	} else {
+		res.status(400).json('error logging in');
+	}
+})
+
+app.post('/register',(req,res)=>{
+	const {name,email,password} = req.body;
+	database.users.push(
+	{
+      	id: '125',
+      	name: name,
+      	email: email,
+      	password: password,
+      	entries: 0,
+      	joined: new Date()
+	})
+	res.json(database.users[database.users.length-1]);
 })
 
 app.listen(3000,()=>{
