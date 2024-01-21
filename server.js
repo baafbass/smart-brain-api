9bc375645e013f5795e1bef80a94761babbf7fbe
Farid <abdoulfaridbassirou7898@gmail.com>
2024-01-21 20:49:37 +0300

    /profile/id and /image

--- server.js
+++ server.js
@@ -53,16 +53,38 @@ app.post('/register',(req,res)=>{
 	res.json(database.users[database.users.length-1]);
 })
 
+app.get('/profile/:id',(req,res)=>{
+	const {id} = req.params;
+	let found = false;
+	database.users.forEach(user => {
+		if (user.id === id)
+		{
+			found =true;
+			return res.json(user);
+		}
+	})
+	if(!found)
+	{
+			res.status(404).json('not found');
+     }
+})
+
+app.post('/image',(req,res)=>{
+	const {id} = req.body;
+	let found =false;
+	database.users.forEach(user => {
+		if (user.id === id)
+		{
+			found =true;
+			user.entries++
+			return res.json(user.entries);
+		}
+	})
+	if(!found){
+		res.status(404).json('No found')
+	}
+})
+
 app.listen(3000,()=>{
 	console.log('app is running on port 3000')
 })
-
-
-
-/*
-/ --> res = this is working
-/ signin --> POST = success/fail
-/ register --> POST = user
-/profile/:userid --> GET = user
-/image --> PUT --> user
-*/
