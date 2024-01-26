e45f02b9217825582ea72feaed598d213cbb3d18
Farid <abdoulfaridbassirou7898@gmail.com>
2024-01-26 11:29:21 +0300

    Updating Entries

--- server.js
+++ server.js
@@ -108,18 +108,13 @@ if(user.lenght){
 
 app.put('/image',(req,res)=>{
 	const {id} = req.body;
-	let found =false;
-	database.users.forEach(user => {
-		if (user.id === id)
-		{
-			found =true;
-			user.entries++
-			return res.json(user.entries);
-		}
-	})
-	if(!found){
-		res.status(404).json('No found')
-	}
+db('users').where('id','=',id)
+.increment('entries',1)
+.returning('entries')
+.then(entries => {
+	res.json(entries[0].entries);
+})
+.catch(err => res.status(400).json('unable to get entries'));
 })
 
 
